19e44eb652ad557761753b48fe5dd40d20c0f75d
Alfre <alfre.roble.aba@gmail.com>
2019-07-21 13:50:05 +0200

    Works fine but without Useful.py
    All functions that i wanted in Useful.py are now in Controllers.py

--- Bot/Controllers.py
+++ Bot/Controllers.py
@@ -3,6 +3,7 @@ import time
 from States import *
 from Useful import *
 from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
+from rlbot.utils.structures.game_data_struct import GameTickPacket
 
 def exampleController(agent,target_object,target_speed): #target_object es un objeto tipo obj 
         
@@ -21,8 +22,6 @@ def exampleController(agent,target_object,target_speed): #target_object es un ob
             controller_state.boost = False
             controller_state.handbrake = True
         controller_state.steer = sign(angle_to_target)*min(1,abs(2*angle_to_target))
-        
-            #controller.Steer = Math.Sign(relativeBotToTargetAngle.Z) * Math.Min(1, Math.Abs(2f * relativeBotToTargetAngle.Z));
 
         #throttle
         '''if target_speed > current_speed:
@@ -56,3 +55,60 @@ def draw_debug(renderer, target):
 
 def velocity2D(target_object):
     return math.sqrt(target_object.velocity.data[0]**2 + target_object.velocity.data[1]**2)
+    
+class Vector3:
+    def __init__(self, data):
+        self.data = data
+    def __sub__(self,value):
+        return Vector3([self.data[0]-value.data[0],self.data[1]-value.data[1],self.data[2]-value.data[2]])
+    def __mul__(self,value):
+        return (self.data[0]*value.data[0] + self.data[1]*value.data[1] + self.data[2]*value.data[2])
+    
+
+class obj:
+    def __init__(self):
+        self.location = Vector3([0,0,0])
+        self.velocity = Vector3([0,0,0])
+        self.rotation = Vector3([0,0,0])
+        self.rvelocity = Vector3([0,0,0])
+        
+        self.local_location = Vector3([0,0,0])
+        self.boost = 0
+#
+def to_local(target_object,our_object):
+    x = (target_object.location - our_object.location) * our_object.matrix[0]
+    y = (target_object.location - our_object.location) * our_object.matrix[1]
+    z = (target_object.location - our_object.location) * our_object.matrix[2]
+    return [x,y,z]
+
+def rotator_to_matrix(our_object):
+    r = our_object.rotation.data
+    CR = math.cos(r[2])
+    SR = math.sin(r[2])
+    CP = math.cos(r[0])
+    SP = math.sin(r[0])
+    CY = math.cos(r[1])
+    SY = math.sin(r[1])
+
+    matrix = []
+    matrix.append(Vector3([CP*CY, CP*SY, SP]))
+    matrix.append(Vector3([CY*SP*SR-CR*SY, SY*SP*SR+CR*CY, -CP * SR]))
+    matrix.append(Vector3([-CR*CY*SP-SR*SY, -CR*SY*SP+SR*CY, CP*CR]))
+    return matrix
+
+def velocity2D(target_object):
+    return math.sqrt(target_object.velocity.data[0]**2 + target_object.velocity.data[1]**2)
+
+def distance2D(target_object, our_object):
+    if isinstance(target_object,Vector3):
+        difference = target_object - our_object
+    else:
+        difference = target_object.location - our_object.location
+    return math.sqrt(difference.data[0]**2 + difference.data[1]**2)
+    
+
+def sign(x):
+    if x<=0:
+        return -1
+    else:
+        return 1
